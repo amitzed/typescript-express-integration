121ca92c27c167dc5d10eeba455377f705c98461
amitzed <amitzmn@gmail.com>
2020-12-31 16:06:36 -0500

    Installed express and several other depenencies and index route successfully working in browser

---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+// Route Handlers
+app.get('/', function (req, res) {
+    res.send("\n    <div>\n      <h1>Testing</h1>\n    </div>\n  ");
+});
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+
+
+const app = express();
+
+// Route Handlers
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Testing</h1>
+    </div>
+  `);
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
